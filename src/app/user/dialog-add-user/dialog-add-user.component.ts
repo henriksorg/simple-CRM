1988addc2aaeccd982e820018adeac7244a877fc
Henrik Sorg <henrik@reif-sorg.de>
2024-03-21 19:58:57 +0100

    add Users to firestore

--- src/app/user/dialog-add-user/dialog-add-user.component.ts
+++ src/app/user/dialog-add-user/dialog-add-user.component.ts
@@ -1,13 +1,16 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../../models/user.class';
 import { FormsModule } from '@angular/forms';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { log } from 'console';
+
 
 
 
@@ -24,10 +27,31 @@ export class DialogAddUserComponent {
   birthDate!: Date;
 
 
-  constructor(){}
+  constructor() { }
+  private firestore: Firestore = inject(Firestore);
 
-  saveUser(){
+
+  saveUser() {
     this.user.birthDate = this.birthDate.getTime();
     console.log(this.user);
+    this.addUser();
+    // collection(this.firestore, 'users').add(this.user).then((result: any) =>{
+    //   console.log(result);
+    // })
+
+  }
+  async addUser(){
+    await addDoc(collection(this.firestore, 'users'), {
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      birthDate: this.user.birthDate,
+      street: this.user.street,
+      zipCode: this.user.zipCode,
+      city: this.user.city
+  });
+  }
+
+  getUsersRef(){
+    return collection(this.firestore, 'users')
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { CommonModule, NgFor } from '@angular/common';
 
 
 
@@ -22,8 +25,12 @@ export class UserComponent {
 
   user = new User();
 
+  firestore: Firestore = inject(Firestore)
+  items$: Observable<any[]>;
+
   constructor(public dialog: MatDialog){
-    
+    const aCollection = collection(this.firestore, 'items')
+    this.items$ = collectionData(aCollection);
   }
 
   openDialog(): void {
